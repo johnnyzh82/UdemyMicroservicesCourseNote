fef7d0d83742e002e4124d6652a74311ee1b6d12
Johnny <johnnyzh8@gmail.com>
2021-01-16 02:06:05 -0800

    fix the shit

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -6,13 +6,15 @@ import { Ticket } from "../models/ticket";
 const router = express.Router();
 
 router.post(
-    '/api/tickets', 
-    requireAuth, 
+    '/api/tickets',
+    requireAuth,
     [
         body('title').not().isEmpty().withMessage('Title is required'),
-        body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
-    ], 
-    validateRequest, 
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price must be greater than 0'),
+    ],
+    validateRequest,
     async (req: Request, res: Response) => {
         const { title, price } = req.body;
 
@@ -20,9 +22,9 @@ router.post(
             title,
             price,
             userId: req.currentUser!.id,
-        })
+        });
         await ticket.save();
-
+    
         res.status(201).send(ticket);
     }
 );
