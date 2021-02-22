92cfc533d4d01139a2a65da8f1d66ae202343e63
Johnny <johnnyzh8@gmail.com>
2021-02-21 17:43:42 -0800

    Updates

--- ticketing/common/src/events/base-publisher.ts
+++ ticketing/common/src/events/base-publisher.ts
@@ -9,8 +9,7 @@ interface Event {
 export abstract class Publisher<T extends Event> {
     abstract subject: T['subject'];
 
-    constructor(private client: Stan) {
-
+    constructor(protected client: Stan) {
     }
 
     publish(data: T['data']): Promise<void> {
